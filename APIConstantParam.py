ccaedd5de591f6dd128cfcf0884d8fa8b15c701a
PKup03 <167389711+PKup03@users.noreply.github.com>
2026-01-27 00:18:05 -0500

    Added (broken) iterate code

--- APIConstantParam.py
+++ APIConstantParam.py
@@ -1,7 +1,9 @@
 ### Live code, check below for test code
 import adsk.core, traceback, adsk.fusion, importlib
 import json, os, adsk.cam
+import math
 from .LCA_Interaction import Parameters
+from .LCA_Interaction import TargetMasses
 #from .LCA_Interaction import 
 
 # global set of event handlers to keep them referenced
@@ -57,7 +59,8 @@ def FindDerivative(paramName):
         ## Set the new value for the global parameter
         importlib.reload(LCA_Interaction) # Reload the LCA_Interaction file to get updated parameter values
         Parameters = LCA_Interaction.Parameters() # Get the updated parameter values from the LCA_Interaction file
-        userParams.itemByName(str(paramName)).expression = str(Parameters[0]) + ' in'
+        userParams.itemByName(str(paramName)).expression = str(userParams[0].value) + ' in'
+
         #Find initial mass of each body in the component
         for j in range(0, root.bRepBodies.count):
             body = root.bRepBodies.item(j)
@@ -66,7 +69,7 @@ def FindDerivative(paramName):
             bodyMasses.append(mass)
         
         #Set the new value for the global parameter
-        userParams.itemByName(str(paramName)).expression = str(Parameters[0]+0.0001) + ' in'
+        userParams.itemByName(str(paramName)).expression = str(userParams[0].value+0.0001) + ' in'
 
         #Find new body masses after parameter change
         for j in range(0, root.bRepBodies.count):
@@ -79,14 +82,60 @@ def FindDerivative(paramName):
         for j in range(0, root.bRepBodies.count):
             BodyDerivatives.append((float(bodyMassesNew[j])-float(bodyMasses[j]))/0.0001)
             #ui.messageBox('Derivative of ' + root.bRepBodies[j].name + ' mass with respect to ' + paramName + ' is ' + str(BodyDerivatives[j]) + ' kg/in')
-        return(BodyDerivatives)
+        return BodyDerivatives
 
 def AllDerivatives():
     currentDerivatives = []
-
     for i in range(0, userParams.count-1):
         currentDerivatives.append(FindDerivative(userParams[i].name))
-        ui.messageBox(str(currentDerivatives))
+    #ui.messageBox(str(currentDerivatives))
+    return currentDerivatives
+
+def iterate():
+    bodyMasses = []
+    for j in range(0, root.bRepBodies.count):
+        body = root.bRepBodies.item(j)
+        physProps = adsk.fusion.PhysicalProperties.cast(body.physicalProperties)
+        mass = unitsMgr.formatInternalValue(physProps.mass, 'kg', False) #Converts mass from document mass units to kg
+        bodyMasses.append(mass)
+
+    currentDerivatives = AllDerivatives()
+    power = 2
+    temp = 1
+
+    importlib.reload(LCA_Interaction) # Reload the LCA_Interaction file to get updated target mass values
+    TargetMasses = LCA_Interaction.TargetMasses() # Get the updated target mass values from the LCA_Interaction file
+    ui.messageBox("Target Masses: " + str(TargetMasses[0]))
+
+    if TargetMasses[0]-float(bodyMasses[0]) == 0:
+        ui.messageBox("check 0")
+        pass
+    elif TargetMasses[0]-float(bodyMasses[0]) > 0 and currentDerivatives[0][0] < 0 or TargetMasses[0]-float(bodyMasses[0]) < 0 and currentDerivatives[0][0] > 0:
+        while TargetMasses[0] - float(bodyMasses[0]) > 10**power:
+            ui.messageBox("check 1")
+            inc = temp*10**(power-1)
+            userParams.itemByName(str(userParams[0].name)).expression = str(userParams[0]+inc) + ' in'
+            if TargetMasses[0]-float(bodyMasses[0]) == 0:
+                ui.messageBox("check 2")
+                pass
+            else:
+                ui.messageBox("check 3")
+                temp = -temp
+                power -= 1
+    else:
+        while TargetMasses[0] - float(bodyMasses[0]) < -(10**power):
+            ui.messageBox("check 4")
+            inc = -temp*10**(power-1)
+            userParams.itemByName(str(userParams[0].name)).expression = str(userParams[0].value+inc) + ' in'
+            if TargetMasses[0]-float(bodyMasses[0]) == 0:
+                ui.messageBox("check 5")
+                pass
+            else:
+                ui.messageBox("check 6")
+                temp = -temp
+                power -= 1
+    ui.messageBox("check 7")
+    PaletteUpdate()
 
 #Event handler for the cameraChanged event to remove the palette, allowing for the Add-In to be re-run without restarting Fusion 360.
 class MyCameraMovedHandler(adsk.core.CameraEventHandler):
@@ -114,10 +163,11 @@ class MyHTMLEventHandler(adsk.core.HTMLEventHandler):
                 PaletteUpdate()
                 #FindDerivative(userParams[1].name)
                 AllDerivatives()
+                iterate()
                 del Firing
                 # ui.messageBox(Firing)
         except:
-            ui.messageBox('Failed:\n{}'.format(traceback.format_exc())) 
+            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
 
 def run(context):
     try:
