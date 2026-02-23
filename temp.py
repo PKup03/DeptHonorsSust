2a094369c57214ae9ca83291e423dccb63d23847
PKup03 <167389711+PKup03@users.noreply.github.com>
2026-02-23 12:36:58 -0500

    Cleaned up code to now be able to add the OpenLCA interaction as a function. File should now iterate when receiving new data from APIConstantParam until a defined number of iterations (at this moment, 10).

--- temp.py
+++ temp.py
@@ -1,14 +1,21 @@
-import os
+import os, csv
 script_dir = os.path.dirname(os.path.abspath(__file__))
 filepath = os.path.join(script_dir, "output.csv")
 
-# while csvlen == len(Combos) and csvlen != 0:
-#                     with open(filepath, 'r') as file:
-#                         my_reader = csv.reader(file, delimiter=',')
-#                         csvlen = len(list(my_reader))
-#                         ui.messageBox(str(csvlen) + " vs " + str(len(Combos)))
-#                     if csvlen != len(Combos) and csvlen != 0:
-#                         GeneratePoints(27, 9)
-#                         ui.messageBox("Data generation complete. Generated " + str(len(Combos)) + " points.")
+## Initializing variables
+csvlen = len(list(csv.reader(open(filepath, 'r', delimiter=','))))
+counter = 1 #Variable to count the number of iterations, limiting the number of iterations
 
-open(filepath, 'w').close()
+## Define OpenLCA function here
+
+## Iteration logic
+while counter == 1 or csvlen == len(Output) and csvlen != 0:
+    if counter == 10: #Checks to see if the loop has iterated enough times
+        open(filepath, 'w').close() #To clear the file
+        break
+    with open(filepath, 'r') as file:
+        my_reader = csv.reader(file, delimiter=',')
+        csvlen = len(list(my_reader))
+    if csvlen != len(Output) and csvlen != 0:
+        OpenLCAFunction() #Function that generates the sustainability data and provides an Output to output.csv
+        counter += 1 #Increments the counter, representing one iteration of the full loop
